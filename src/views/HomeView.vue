a2833fe6dbe45aff3a4f8e7d3159ac169122f54d
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-02-04 12:54:46 +0800

    add: refs and computed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <p ref="p">Name {{ name }} and {{ age }}</p>
+    <button @click="handleClick">Click</button>
+    <input type="text" v-model="name" />
+   -->
+   <input type="text" v-model="search" />
+   <p>{{ search }}</p>
+   <div v-for="name in matchingNames" :key="name">
+    {{ name }}
+   </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { computed, ref } from 'vue';
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    // const p = ref(null)
+
+    // const name = ref('Mario');
+    // const age = ref(10);
+
+    // const handleClick = () => {
+    //   console.log(p, p.value)
+    //   name.value = 'test'
+    //   age.value = 25
+    // }
+
+    // return { name, age, handleClick, p }
+
+    const names = ref(['max', 'alhihi', 'nahida', 'kaeya'])
+    const search = ref('')
+
+    const matchingNames = computed(() => {
+      return names.value.filter((item) => item.includes(search.value))
+    })
+
+    return { names, search, matchingNames }
+
   }
 }
 </script>
